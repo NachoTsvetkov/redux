6e51a643c9a4de6c4e3db7b82f4e738711111c78
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2018-09-05 16:41:04 +0300

    configure store implemented

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { applyMiddleware, createStore, compose } from 'redux'
 import Root from './components/Root'
-import thunkMiddleware from 'redux-thunk'
-import todoApp from './reducers/index'
-import loggerMiddleware from './middleware/logger'
-import monitorReducerEnhancer from './enhansers/monitorReducer'
+import configureStore from './configureStore'
 
-const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware);
-const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer);
-const store = createStore(todoApp, undefined, composedEnhancers); 
+const store = configureStore();
 
 render(
   <Root store={store} />,
